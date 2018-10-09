85cd5efb2176d60d51d02543ed2c1f823c15d28f
Sydney Grant <sg3957@nyu.edu>
2018-10-09 21:56:46 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,15 +1,16 @@
 class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+ # attr_reader :brand
 
-attr_accessor :brand, :color, :size, :material, :condition
+ 
+  def initialize(brand)
+    @brand = brand
+  end
+  
 
-
-def initialize(brand)
- @brand = brand
-end
-
-def cobble
- self.condition = “new”
-puts “Your shoe is as good as new!”
-end
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
 
 end
