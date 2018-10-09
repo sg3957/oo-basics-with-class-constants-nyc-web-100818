6acf75e97cc04120355396a83d304297bf4c38da
Sydney Grant <sg3957@nyu.edu>
2018-10-09 21:49:39 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,20 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
+  attr_accessor :color, :size, :material, :condition, :brand
   attr_reader :brand
 
+  BRANDS = []
   def initialize(brand)
     @brand = brand
+  end
+  
+  def brand=(brand)
+    @brand = brand
+    BRANDS.each do |brand|
+      if !(BRANDS.include?(brand))
+    BRANDS << brand 
+  end
+  end
+  BRANDS
   end
 
   def cobble
